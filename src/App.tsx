ee8cb7c60ce8c14152a3d8cbfc65c57267eac881
thaominh97 <85306390+thaominh97@users.noreply.github.com>
2021-12-06 14:16:22 +0700

    2 changes add ex in useReducerEx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
 import "./styles.css";
-import { useState, useCallback } from "react";
+import { useState, useCallback, useReducer } from "react";
 import { UseRefEx } from "./components/index";
 import ReactMemoEx from "./components/reactMemoEx";
 import UseCallbackEx from "./components/useCallbackEx";
 import UseMemoEx from "./components/useMemoEx";
 import UseReducerEx from "./components/useReducerEx";
 
+const initState = 0;
+
+const DOWN_ACTION = "down";
+const UP_ACTION = "up";
+
+const reducer = (state: any, action: any) => {
+  console.log("reducer runing...");
+  switch (action) {
+    case UP_ACTION:
+      return state + 1;
+    case DOWN_ACTION:
+      return state - 1;
+    default:
+      return new Error("Invalid action");
+  }
+};
+
 export default function App() {
   const [count, setCount] = useState(0);
+  const [count1, dispatch] = useReducer(reducer, initState);
   const handleIncrease = useCallback(() => {
     setCount((prevCount: number) => prevCount + 1);
   }, []);
+  const handleCountUp = () => dispatch(UP_ACTION);
+  const handleCountDown = () => dispatch(DOWN_ACTION);
 
   return (
     <div className="App">
@@ -21,7 +41,11 @@ export default function App() {
       <UseCallbackEx onClick={handleIncrease} />
       <p>{count}</p>
       <UseMemoEx />
-      <UseReducerEx />
+      <UseReducerEx
+        count={count1}
+        onCountDown={handleCountDown}
+        onCountUp={handleCountUp}
+      />
     </div>
   );
 }
--- src/components/useReducerEx.tsx
+++ src/components/useReducerEx.tsx
@@ -1,19 +1,39 @@
 import React from "react";
 
-const UseReducerEx: React.FC = () => {
+interface PropsType {
+  count: number;
+  onCountUp: any;
+  onCountDown: any;
+}
+
+const UseReducerEx: React.FC<PropsType> = ({
+  count,
+  onCountDown,
+  onCountUp
+}) => {
   return (
     <>
       <h3>5. useReducer</h3>
       <p>
         - An alternative to useState. Accepts a reducer of type (state, action)
-        => newState, and returns the current state paired with a dispatch
+        {`=>`} newState, and returns the current state paired with a dispatch
         method.
         <br />
         - useReducer is usually preferable to useState when you have complex
         state logic that involves multiple sub-values or when the next state
         depends on the previous one.
         <br />
+        useState and useReducer are different:
+        <br />- useState: init state {`>`} action.
+        <br />- useReducer: init state {`>`} action {`>`} reducer {`>`}{" "}
+        dispatch.
+        <br />
+        Ex:
+        <br />
+        {count}
       </p>
+      <button onClick={onCountUp}>Up</button>
+      <button onClick={onCountDown}>Down</button>
     </>
   );
 };
